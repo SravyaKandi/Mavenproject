24d7e7360b74c48ca0a8c5b98f6d466859c8550b
sravya <kandisravya1@gmail.com>
2024-11-17 21:12:24 +0530

    Sample Maven Java

---
+++ src/main/java/com/app/demo/Mavenpro/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.Mavenpro;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("my name is Sravya,roll number is 22BD1A664X");
+    }
+}
